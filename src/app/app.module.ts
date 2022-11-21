4a0c51267e5bcd1dd4c3f09f1093e9db3cda0336
furyy-j <96385920+furyy-j@users.noreply.github.com>
2022-11-21 11:06:53 +0200

    created the main architecture of the management page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ManagementPageComponent } from './management-page/management-page.component';
+import { QuestionComponent } from './management-page/question-card/question.component';
+import { CreateCardComponent } from './management-page/create-card/create-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManagementPageComponent,
+    QuestionComponent,
+    CreateCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/management-page/question-card/question.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-question-card',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.scss']
+})
+export class QuestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
