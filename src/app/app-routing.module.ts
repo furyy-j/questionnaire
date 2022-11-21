3591f475792bbee6bd0bf376beb0f56f9d1613f1
furyy-j <96385920+furyy-j@users.noreply.github.com>
2022-11-21 11:24:31 +0200

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ManagementPageComponent} from "./management-page/management-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: '/management'
+  },
+  {
+    path: 'management', component: ManagementPageComponent
+  }]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ManagementPageComponent } from './management-page/management-page.component';
 import { QuestionComponent } from './management-page/question-card/question.component';
 import { CreateCardComponent } from './management-page/create-card/create-card.component';
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { CreateCardComponent } from './management-page/create-card/create-card.c
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
