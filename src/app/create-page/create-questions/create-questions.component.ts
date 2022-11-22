76acfb9ec15f36a13032205608563cabe49f2010
furyy-j <96385920+furyy-j@users.noreply.github.com>
2022-11-22 12:08:26 +0200

    created question form

--- src/app/create-page/create-questions/create-questions.component.ts
+++ src/app/create-page/create-questions/create-questions.component.ts
@@ -1,20 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Question} from "../../common/models/question.interface";
 import {Type} from "../../common/enums/types.enum";
+import {Subscription} from "rxjs";
+
 
 @Component({
   selector: 'app-create-questions',
   templateUrl: './create-questions.component.html',
   styleUrls: ['./create-questions.component.scss']
 })
-export class CreateQuestionsComponent implements OnInit {
+export class CreateQuestionsComponent implements OnInit, OnDestroy {
 
+  constructor(private formBuilder: FormBuilder) {
 
-  constructor(private formBuilder: FormBuilder) { }
+    this.sub = this.form.controls.type.valueChanges.subscribe(() => {
+      !this.hasAnswers ? this.form.controls.answers.setErrors(null) : this.form.controls.answers.updateValueAndValidity();
+    });
+  }
 
+  sub!: Subscription;
+
+  questionType = Type;
+  types = Object.keys(Type);
+
+  form = this.formBuilder.group({
+    title: new FormControl(null, [Validators.required]),
+    type: new FormControl(null, [Validators.required]),
+    answers: new FormArray([])
+  })
 
   ngOnInit(): void {
+
   }
+  get hasAnswers(): boolean | null{
+    const value = this.form.controls.type.value;
+    console.log(value !== this.questionType.Open)
+    console.log(value && value !== this.questionType.Open)
+
+    return value && value !== this.questionType.Open;
+  }
+  createQuestion(){}
+
+
+  ngOnDestroy() {
+    if(this.sub){this.sub.unsubscribe()}
+  }
+
 
 }
